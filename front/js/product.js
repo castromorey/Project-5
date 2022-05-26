b49cb6321b627f15b14324037c37229d9dc0901a
castromorey <castromorey@gmail.com>
2022-05-26 16:42:18 -0400

    working in product page to validate data

--- front/js/product.js
+++ front/js/product.js
@@ -1,37 +1,52 @@
 
+const colorsElm = document.querySelector('#colors');
+const qtyElm = document.querySelector('#quantity');
+
+const searchParams = new URLSearchParams(location.search);
+const productId = searchParams.get('id')
 
 function fetchProduct(){
-    fetch('http://localhost:3000/api/products/415b7cacb65d43b2b5c1ff70f3393ad1').then(res=>res.json()).then(product=>{
+    fetch('http://localhost:3000/api/products/' + productId).then(res=>res.json()).then(product=>{
         
         document.querySelector('#title').innerHTML=product.name;
         document.querySelector('#price').innerHTML=product.price;
         document.querySelector('#description').innerHTML=product.description;
-        document.querySelector(".item__img").innerHTML=`<img src="${product.imageUrl}" alt="" />`;
+        document.querySelector(".item__img").innerHTML=`<img src="${product.imageUrl}" alt="${product.altTxt}" />`;
         
-
-    
-            
-    
+        product.colors.map((color) => {
+            colorsElm.innerHTML+=`<option value="${color}">${color}</option>`;
+        });
+        
+        
     })
 }
 
 fetchProduct();
 
-/*
-prueba
+const addToCart = () => {
 
-let url = 'http://localhost:3000/api/products';
-  fetch(url)
-      .then( response => res.json() )
-      .then( data => showData(data) )
-      .catch( error => console.log(error) )
 
-  const showData = (data) => {
-      console.log(data)
-      let body = ""
-      for (var i = 0; i < data.length; i++) {      
-         body+=`<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].email}</td></tr>`
-      }
-      document.getElementById('data').innerHTML = body
- 
-  }*/
+
+    const data = {
+        productId,
+        color: colorsElm.value,
+        qty: Number(qtyElm.value)
+    }
+
+    const errors = {};
+
+    if(!data.color) errors.color = 'Must select a color.'
+    if(!data.qty) errors.qty = 'Quantity must be 1 or more'
+
+    if(Object.keys(errors).length === 0 ) {
+        const cart = JSON.parse(localStorage.getItem('cart'));
+        localStorage.setItem('cart', JSON.stringify([...cart, data]))
+    }else{
+        console.log({errors})
+    }
+
+}
+
+document.querySelector('#addToCart').addEventListener('click', addToCart );
+
+
